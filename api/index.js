9a7c845d194e31ee137dba9dbea9dd4a0965e1f2
shashank1325 <shashank.u1312@gmail.com>
2024-02-12 20:27:54 +0530

    mongoose connected

--- api/index.js
+++ api/index.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(bodyparser.json())
 const port = 5000;
 
-const dbURI = process.env.MONGO
-mongoose.connect(dbURI)
+mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log("MongoDB connected successfully...!");
     app.listen(port, () => console.log(`Server running on port ${port}!`));
